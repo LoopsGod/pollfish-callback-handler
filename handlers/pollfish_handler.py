a18642cba6651d009dd404faab1d4ff734717637
N.J. van Riet <noah.van.riet@hva.nl>
2019-05-19 02:18:25 +0200

    // add

---
+++ handlers/pollfish_handler.py
@@ -0,0 +1,11 @@
+#
+# Verify a completed survey from S2S Callbacks
+#
+import flask
+
+
+def verify():
+    argument1 = flask.request.args.get('YOURARGUMENT1')
+    argument2 = flask.request.args.get('YOURARGUMENT2')
+
+# TODO Make handler code here
