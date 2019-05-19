18a996546bbef80fd544090899213bc3a89e432f
Noah <26509744+josh1dev@users.noreply.github.com>
2019-05-19 02:24:02 +0200

    // dependency

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
+# Include the handler method(s)
+from handlers.pollfish_handler import verify
+
 app = Flask(__name__)
 
 
 @app.route('/pollfish-webhook')
 def verify_survey():
-    return 'Hello World!'
+    return verify()
 
 
 if __name__ == '__main__':
